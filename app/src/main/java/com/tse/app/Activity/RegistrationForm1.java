2cdfcd0469d1603ed4e3ae604e4da8ba1ac894bb
Vijay Dave <vijaydave117@gmail.com>
2018-12-15 10:46:10 +0530

    Initial commit

--- app/src/main/java/com/tse/app/Activity/RegistrationForm1.java
+++ app/src/main/java/com/tse/app/Activity/RegistrationForm1.java
@@ -1,7 +1,9 @@
 package com.tse.app.Activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.tse.app.R;
 
@@ -11,5 +13,11 @@ public class RegistrationForm1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_form1);
+    }
+
+    public void MoveRegistrationform2(View view) {
+        Intent intent = new Intent(RegistrationForm1.this,RegistrationForm2.class);
+        startActivity(intent);
+
     }
 }
---
+++ app/src/main/java/com/tse/app/Activity/RegistrationForm2.java
@@ -0,0 +1,15 @@
+package com.tse.app.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tse.app.R;
+
+public class RegistrationForm2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_form2);
+    }
+}

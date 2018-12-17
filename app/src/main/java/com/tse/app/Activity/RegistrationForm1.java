10ee13d09697021cfb0750a5b4406636e65348b8
Vijay Dave <vijaydave117@gmail.com>
2018-12-17 12:23:29 +0530

    Initial commit

--- app/src/main/java/com/tse/app/Activity/RegistrationForm1.java
+++ app/src/main/java/com/tse/app/Activity/RegistrationForm1.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.RadioButton;
 
 import com.tse.app.R;
@@ -11,6 +12,7 @@ import com.tse.app.R;
 public class RegistrationForm1 extends AppCompatActivity {
 
     RadioButton rbRegisterManufature, rbRegisterTraders, rbRegisterService, rbRegisterBroker;
+    EditText edRegistrationfirstName,edRegistrationLastName, edRegistrationEmail, edRegistrationPassword, edRegistrationCnfirmPassword, edRegistrationcEnterprice, edRegistrationGstNo, edRegistrationPanNo;
 
 
     @Override
@@ -63,16 +65,69 @@ public class RegistrationForm1 extends AppCompatActivity {
     }
 
     private void findViewByIdS() {
+
         rbRegisterManufature = (RadioButton) findViewById(R.id.rb_register_manufacture);
         rbRegisterTraders = (RadioButton) findViewById(R.id.rb_register_traders);
         rbRegisterService = (RadioButton) findViewById(R.id.rb_register_service);
         rbRegisterBroker = (RadioButton) findViewById(R.id.rb_register_broker);
-    }
+        edRegistrationfirstName = (EditText) findViewById(R.id.edRegistrationfirstName);
+        edRegistrationLastName = (EditText) findViewById(R.id.edRegistrationLastName);
+        edRegistrationEmail = (EditText) findViewById(R.id.edRegistrationEmail);
+        edRegistrationPassword = (EditText) findViewById(R.id.edRegistrationPassword);
+        edRegistrationCnfirmPassword = (EditText) findViewById(R.id.edRegistrationCnfirmPassword);
+        edRegistrationcEnterprice = (EditText) findViewById(R.id.edRegistrationcEnterprice);
+        edRegistrationGstNo = (EditText) findViewById(R.id.edRegistrationGstNo);
+        edRegistrationPanNo = (EditText) findViewById(R.id.edRegistrationPanNo);
 
+    }
 
 
     public void MoveRegistrationform2(View view) {
-        Intent intent = new Intent(getApplicationContext(), RegistrationForm2.class);
-        startActivity(intent);
+        if (validation()) {
+            Intent intent = new Intent(getApplicationContext(), RegistrationForm2.class);
+            startActivity(intent);
+       }
+
+
     }
+
+    public boolean validation() {
+        if (edRegistrationfirstName.getText().toString().length() == 0) {
+            edRegistrationfirstName.setError("Please enter First Name");
+            return false;
+        } else if (edRegistrationLastName.getText().toString().length() == 0) {
+            edRegistrationLastName.setError("Please enter Last Name");
+            return false;
+        }
+
+        else if (edRegistrationEmail.getText().toString().length() == 0) {
+            edRegistrationEmail.setError("Please enter Email");
+            return false;
+        } else if (!edRegistrationEmail.getText().toString().matches("^[a-zA-Z0-9_\\+-]+(\\.[a-zA-Z0-9_\\+-]+)*@[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*\\.([a-zA-Z]{2,4})$")) {
+            edRegistrationEmail.setError("email not match");
+            return false;
+
+
+        } else if (edRegistrationPassword.getText().toString().length() == 0) {
+            edRegistrationPassword.setError("Please enter Password");
+            return false;
+        } else if (edRegistrationCnfirmPassword.getText().toString().length() == 0) {
+            edRegistrationCnfirmPassword.setError("Please enter Password");
+            return false;
+
+        } else if (edRegistrationcEnterprice.getText().toString().length() == 0) {
+            edRegistrationcEnterprice.setError("Please enter EnterPrice");
+            return false;
+        } else if (edRegistrationGstNo.getText().toString().length() == 0) {
+            edRegistrationGstNo.setError("Please enter GST NO.");
+            return false;
+
+        } else if (edRegistrationPanNo.getText().toString().length() == 0) {
+            edRegistrationPanNo.setError("Please enter PAN NO.");
+            return false;
+
+        }
+        return true;
+    }
+
 }
--- app/src/main/java/com/tse/app/Activity/RegistrationForm2.java
+++ app/src/main/java/com/tse/app/Activity/RegistrationForm2.java
@@ -5,25 +5,107 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Spinner;
 
 import com.tse.app.R;
 
 public class RegistrationForm2 extends AppCompatActivity {
-    Spinner spinner;
+    Spinner spRegistrationSelectCategory,spRegistrationMeters;
+    EditText edRegistrationAddress1,edRegistrationArea1,edRegistrationPincode1,edRegistrationAddress2,
+            edRegistrationArea2,edRegistrationPincode2,edRegistrationContactNo1,edRegistrationContactNo2,
+            edRegistrationSubCategory,edRegistrationQuantity,edRegistrationReferralCode;
+    RadioButton rbRegistrationGeneral,rbRegistrationPrime;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_form2);
-        spinner = findViewById(R.id.generalSpinner);
+
+        Findviewby_Id();
 
 
     }
 
+    private void Findviewby_Id() {
+        spRegistrationSelectCategory = (Spinner) findViewById(R.id.spRegistrationSelectCategory);
+        spRegistrationMeters = (Spinner) findViewById(R.id.spRegistrationMeters);
+        rbRegistrationGeneral = (RadioButton) findViewById(R.id.rbRegistrationGeneral);
+        rbRegistrationPrime = (RadioButton) findViewById(R.id.rbRegistrationPrime);
+        edRegistrationAddress1 =(EditText) findViewById(R.id.edRegistrationAddress1);
+        edRegistrationArea1 = (EditText) findViewById(R.id.edRegistrationArea1);
+        edRegistrationPincode1 = (EditText) findViewById(R.id.edRegistrationPincode1);
+        edRegistrationAddress2 = (EditText) findViewById(R.id.edRegistrationAddress2);
+        edRegistrationArea2 = (EditText) findViewById(R.id.edRegistrationArea2);
+        edRegistrationPincode2 = (EditText) findViewById(R.id.edRegistrationPincode2);
+        edRegistrationContactNo1 = (EditText) findViewById(R.id.edRegistrationContactNo1);
+        edRegistrationContactNo2 = (EditText) findViewById(R.id.edRegistrationContactNo2);
+        edRegistrationSubCategory = (EditText) findViewById(R.id.edRegistrationSubCategory);
+        edRegistrationQuantity = (EditText) findViewById(R.id.edRegistrationQuantity);
+        edRegistrationReferralCode = (EditText) findViewById(R.id.edRegistrationReferralCode);
+    }
+
     public void movelogin(View view) {
-        Intent intent = new Intent(RegistrationForm2.this,LoginActivity.class);
-        startActivity(intent);
+        if (Validation()){
+            Intent intent = new Intent(RegistrationForm2.this,LoginActivity.class);
+            startActivity(intent);
+        }
+
+
+    }
+    public boolean Validation(){
+        if (edRegistrationAddress1.getText().toString().length()==0){
+            edRegistrationAddress1.setError("Please Enter Address 1");
+            return false;
+        }
+        else if (edRegistrationArea1.getText().toString().length()==0){
+            edRegistrationArea1.setError("Please Enter Area 1");
+            return false;
+        }
+        else if (edRegistrationPincode1.getText().toString().length()==0){
+            edRegistrationPincode1.setError("Please Enter Pincode 1");
+            return false;
+        }
+        else if (edRegistrationAddress2.getText().toString().length()==0){
+            edRegistrationAddress2.setError("Please Enter Address 2");
+            return false;
+        }
+        else if (edRegistrationArea2.getText().toString().length()==0){
+            edRegistrationArea2.setError("Please Enter Area 2");
+            return false;
+        }
+        else if (edRegistrationPincode2.getText().toString().length()==0){
+            edRegistrationPincode2.setError("Please Enter Pincode 2");
+            return false;
+        }
+        else if (edRegistrationAddress1.getText().toString().length()==0){
+            edRegistrationAddress1.setError("Please Enter Address 1");
+            return false;
+        }
+        else if (edRegistrationContactNo1.getText().toString().length()==0){
+            edRegistrationContactNo1.setError("Please Enter Contact No. 1");
+            return false;
+        }
+        else if (edRegistrationContactNo2.getText().toString().length()==0){
+            edRegistrationContactNo2.setError("Please Enter  Contact No. 2");
+            return false;
+        }
+        else if (edRegistrationSubCategory.getText().toString().length()==0){
+            edRegistrationSubCategory.setError("Please Enter Sub category");
+            return false;
+        }
+        else if (edRegistrationQuantity.getText().toString().length()==0){
+            edRegistrationQuantity.setError("Please Enter Quantity");
+            return false;
+        } else if (edRegistrationReferralCode.getText().toString().length()==0){
+            edRegistrationReferralCode.setError("Please Enter Referral Code");
+            return false;
+        }
+        return true;
 
     }
 }
